6ac3ce180c2eff4b80fbf360d497f0a934e9daa9
Afforix <plch.matej@gmail.com>
2023-03-19 18:57:46 +0100

    initial presets

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -5,3 +5,9 @@ target_compile_options(enable-warnings INTERFACE -Wall -Wextra)
 # link with this interface library to disable warnings, used for extern libraries
 add_library(disable-warnings INTERFACE)
 target_compile_options(disable-warnings INTERFACE -w)
+
+# Fail build if there are warnings, for CI pipelines.
+option(ENABLE_WERROR "Treat warnings as errors." OFF)
+if (ENABLE_WERROR)
+    target_compile_options(enable-warnings INTERFACE -Werror)
+endif()
