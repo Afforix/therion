ceb908da229fe461f14fcee4d070009b50d882fd
Afforix <plch.matej@gmail.com>
2024-08-17 11:56:45 +0200

    extern template fmt::detail::vprintf

--- fmt_printf.h
+++ fmt_printf.h
@@ -2,6 +2,8 @@
 
 #include <fmt/printf.h>
 
+extern template void fmt::detail::vprintf<char, fmt::basic_printf_context<char>>(buffer<char>& buf, basic_string_view<char> format,
+             basic_format_args<fmt::basic_printf_context<char>> args);
 extern template int fmt::vfprintf<char>(
     std::FILE* f, basic_string_view<char> fmt,
     basic_format_args<basic_printf_context<type_identity_t<char>>> args);
--- therion.cxx
+++ therion.cxx
@@ -153,6 +153,8 @@ void thexit(int exit_code)
   exit(exit_code);
 }
 
+template void fmt::detail::vprintf<char, fmt::basic_printf_context<char>>(buffer<char>& buf, basic_string_view<char> format,
+             basic_format_args<fmt::basic_printf_context<char>> args);
 template int fmt::vfprintf<char>(
     std::FILE* f, basic_string_view<char> fmt,
     basic_format_args<basic_printf_context<type_identity_t<char>>> args);
