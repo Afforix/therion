4deaf8cdd67fcd611afc3c87016c3131323ba4f4
Matěj Plch <plch.matej@gmail.com>
2026-05-06 13:37:39 +0200

    install libmagick++-dev on Ubuntu

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -12,7 +12,7 @@ endif()
 if (BUILD_THERION)
     include(PROJ)
     include(Shapelib)
-    find_package(ImageMagick REQUIRED)
+    find_package(ImageMagick COMPONENTS Magick++ REQUIRED)
 endif()
 
 # loch dependencies
